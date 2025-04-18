fe87b95554e3d34179ff492d6eec28916e33de0f
arieldelg <arieldelgrande@hotmail.com>
2025-04-18 09:34:53 -0700

    feat: add class-transformer and class-validator dependencies, implement DTO validation in auth module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config/envs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
@@ -14,7 +15,12 @@ async function bootstrap() {
       },
     },
   );
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen();
 }
 bootstrap();
